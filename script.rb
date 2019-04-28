2a099e19a76d98a3108c318831c1b7239238dfa3
Edward Robinson <erobinsondev@gmail.com>
2019-04-28 16:35:35 +0100

    Add 2 functions. getting of the data and scrapping the data

--- script.rb
+++ script.rb
@@ -2,4 +2,14 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
+def get_jobs
+    base_url = 'http://www.wordthink.com'
+    data = data_scraper(base_url)
 
+end
+
+def data_scraper(url)
+  Nokogiri::HTML(open(url))
+end
+
+puts get_jobs
