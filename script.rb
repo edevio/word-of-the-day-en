bbe76a1cf8ce57d14e77da9aa94797481f8fc704
Edward Robinson <erobinsondev@gmail.com>
2019-04-28 17:57:36 +0100

    Add title and description to array

--- script.rb
+++ script.rb
@@ -5,6 +5,18 @@ require 'pry'
 def get_jobs
     base_url = 'http://www.wordthink.com'
     data = data_scraper(base_url)
+    title = []
+    all_sections = data.css('.category-daily-word')
+
+    all_sections.each_with_index do| section, section_index|
+
+      title << {
+                title: section.css('.title > a').text,
+                description: section.css('p').text
+              }
+    end
+
+    print(title)
 
 end
 
@@ -12,4 +24,4 @@ def data_scraper(url)
   Nokogiri::HTML(open(url))
 end
 
-puts get_jobs
+get_jobs
