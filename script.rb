21f2e6b45001ace4fd3567255873918a24a74ce6
Edward Robinson <erobinsondev@gmail.com>
2019-04-28 19:32:57 +0100

    Write to csv file in anki format
    Adds a method to write to a csv file with the 2 minimum required
    columns; front of card and back of card

--- script.rb
+++ script.rb
@@ -1,27 +1,37 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
+require 'csv'
 
 def get_jobs
     base_url = 'http://www.wordthink.com'
     data = data_scraper(base_url)
-    title = []
+    cards = []
     all_sections = data.css('.category-daily-word')
 
     all_sections.each_with_index do| section, section_index|
 
-      title << {
+      cards << {
                 title: section.css('.title > a').text,
                 description: section.css('p').text
               }
     end
 
-    print(title)
+    write_csv(cards)
 
 end
 
 def data_scraper(url)
   Nokogiri::HTML(open(url))
+end
+
+def write_csv(cards)
+  CSV.open("latest_cards.csv", "w") do |csv|
+    csv << ["back", "front"]
+    cards.each do | card|
+    csv << [card[:title], card[:description]]
+    end
+  end
 end
 
 get_jobs
